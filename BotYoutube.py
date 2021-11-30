52317ab6fe85be3595fdcecfaba1cd284eb7f7a5
LucasBergaminAX4B <lucas.santos@ax4b.com>
2021-11-30 00:36:38 -0300

    PYTUBE

--- BotYoutube.py
+++ BotYoutube.py
@@ -45,11 +45,9 @@ def id(mensagem):
     return mensagem.chat.id
 
 
-def validarURL(mensagem):
-    if mensagem.text[0:8] == 'https://' or mensagem.text[0:7] == 'youtube' or mensagem.text[0:8] == 'youtu.be':
-        dowloandVideo(mensagem)
-    else:
-        texto=f"""Olá {mensagem.chat.first_name} {mensagem.chat.last_name} seja bem vindo a o Bot do youtube :)
+@bot.message_handler(func=validar)
+def mensagem(mensagem):
+            texto=f"""Olá {mensagem.chat.first_name} {mensagem.chat.last_name} seja bem vindo a o Bot do youtube :)
         
 Você pode baixar um vídeo ou apenas uma música
 Para baixar apenas o vídeo digite 
@@ -58,11 +56,6 @@ Ou para baixar apenas a música digite
 --> /musica
 """
         bot.reply_to(mensagem, texto)
-
-
-@bot.message_handler(func=validar)
-def mensagem(mensagem):
-    validarURL(mensagem)
 
 
 bot.polling()
